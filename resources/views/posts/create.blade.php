cb95bc756c9c598f1805a69d4a65f48b00eebc48
Alessandro Caiti <acaiti@hotmail.it>
2020-05-21 17:01:20 +0200

    aggiunto l'error

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,6 +6,15 @@
         <title></title>
     </head>
     <body>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <form action="{{route('posts.store')}}" method="post">
             @csrf
             @method('POST')
